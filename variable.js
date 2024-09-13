e23a22fec99e2aa1c3f9b7e58ebc937a69f26d66
angelpaligar <apaligar07@gmail.com>
2024-09-13 13:57:25 +0800

    Create variable.js

---
+++ variable.js
@@ -0,0 +1,12 @@
+let name = "Angelika L. Paligar";
+
+let WhatDoYouWannaBecomeInYourLife = "programmer";
+
+let gender = "female";
+
+let twitterhandle = "itsmeangel_";
+
+console.log(name);
+console.log(WhatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterhandle);
