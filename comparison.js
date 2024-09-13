25e026430f9d2c5da92c3278cbba1b7b17f626f3
angelpaligar <apaligar07@gmail.com>
2024-09-13 14:03:08 +0800

    Create comparison.js

---
+++ comparison.js
@@ -0,0 +1,30 @@
+let firstFavNum = 19
+let secondFavNum = 4
+
+console.log("Greater than:");
+console.log(19 > 4);
+
+console.log("Less than:");
+console.log(19 < 4);
+
+console.log("Greater than or equal to:");
+console.log(19 >= 4);
+
+console.log("Less than or equal to:");
+console.log(19 <= 4);
+
+console.log("Strict equality (===):");
+console.log(19 === 4)
+console.log("Loose equality (==):");
+console.log(19 == 4);
+
+console.log("Strict inequality (!==):");
+console.log(19 !== 4);
+
+console.log("Loose inequality (!=):");
+console.log(19 != 4);
+
+console.log("AND:");
+console.log((19 > 4) && (19 < 4));
+
+console.log("OR:");
