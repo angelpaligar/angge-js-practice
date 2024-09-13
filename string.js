6bacb87a7a3702a42b7b4d94c88bac9cc0afb2a9
angelpaligar <apaligar07@gmail.com>
2024-09-13 14:06:36 +0800

    Create string.js

---
+++ string.js
@@ -0,0 +1,8 @@
+let favActorFirstName = "megan";
+let favActorLastName = "fox";
+let fullName = favActorFirstName + "" + favActorLastName;
+let uppercase = fullname.toUpperCase();
+let lowercase = "kenny".toLowerCase();
+let message = "My favorite actor is " + uppercase +. His best show is transformers.";
+
+console.log(message);
