5151b65ce5419ae40adb71b2461731634488691a
angelpaligar <apaligar07@gmail.com>
2024-09-13 13:59:04 +0800

    Create number.js

---
+++ number.js
@@ -0,0 +1,9 @@
+let firstFavNum = 19;
+let secondFavNum = 4;
+
+console.log(firstFavNum + secondFavNum);
+console.log(firstFavNum - secondFavNum);
+console.log(firstFavNum * secondFavNum);
+console.log(firstFavNum / secondFavNum);
+console.log(firstFavNum % secondFavNum);
+console.log(firstFavNum ** secondFavNum);
